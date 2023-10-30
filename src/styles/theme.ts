d44aa1877fdad8681ad277c2cb6bf4fa2205328e
arin00github <teru325@naver.com>
2023-10-30 15:31:12 +0900

    💄 Style: chakra-ui 스타일 설정
    - extendTheme 설정 구체화

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,7 @@
+import { colors } from "./colors";
+import { components } from "./components";
+import { styles } from "./global";
+
 import { ThemeConfig, extendTheme } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
@@ -5,6 +9,6 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 };
 
-const baseTheme = extendTheme({ config });
+const baseTheme = extendTheme({ config, colors, styles, components });
 
 export { baseTheme };
