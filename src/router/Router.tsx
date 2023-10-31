b668ba4e78dedea0c0d44cb7c4ee7865e6536f74
arin00github <teru325@naver.com>
2023-10-31 10:39:05 +0900

    🚚 Rename: 아토믹 디자인 시스템 추가 & 이름 변경
    - security -> secure로 변경

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,8 +1,8 @@
 import SafeTreeControlPage from "../pages/safeTree/ControlPage";
 import SafeTreeMonitorPage from "../pages/safeTree/MonitorPage";
-import SecurityGuardControlPage from "../pages/securityGuard/ControlPage";
-import SecurityGaurdMonitorPage from "../pages/securityGuard/MonitorPage";
-import { CONTROL, MONITOR, SAFE_TREE, SECURITY_GUARD } from "../utils/constant";
+import SecureGuardControlPage from "../pages/secureGuard/ControlPage";
+import SecureGaurdMonitorPage from "../pages/secureGuard/MonitorPage";
+import { CONTROL, MONITOR, SAFE_TREE, SECURE_GUARD } from "../utils/constant";
 
 import Root from "./Root";
 
@@ -11,31 +11,31 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 const base_url = import.meta.env.VITE_BASE_URL;
 
 export const pagesArray = [
-  {
-    path: `/${SECURITY_GUARD}/${MONITOR}`,
-    element: <SecurityGaurdMonitorPage />,
-  },
-  {
-    path: `/${SECURITY_GUARD}/${CONTROL}`,
-    element: <SecurityGuardControlPage />,
-  },
-  { path: `/${SAFE_TREE}/${MONITOR}`, element: <SafeTreeMonitorPage /> },
-  { path: `/${SAFE_TREE}/${CONTROL}`, element: <SafeTreeControlPage /> },
+    {
+        path: `/${SECURE_GUARD}/${MONITOR}`,
+        element: <SecureGaurdMonitorPage />,
+    },
+    {
+        path: `/${SECURE_GUARD}/${CONTROL}`,
+        element: <SecureGuardControlPage />,
+    },
+    { path: `/${SAFE_TREE}/${MONITOR}`, element: <SafeTreeMonitorPage /> },
+    { path: `/${SAFE_TREE}/${CONTROL}`, element: <SafeTreeControlPage /> },
 ];
 
 const router = createBrowserRouter(
-  [
-    { element: <Root />, children: pagesArray },
-    {
-      path: "/",
-      element: <Navigate replace to={`/${SECURITY_GUARD}/${CONTROL}`} />,
-    },
-    {
-      path: "*",
-      element: <Navigate replace to={`/${SECURITY_GUARD}/${CONTROL}`} />,
-    },
-  ],
-  { basename: base_url },
+    [
+        { element: <Root />, children: pagesArray },
+        {
+            path: "/",
+            element: <Navigate replace to={`/${SECURE_GUARD}/${CONTROL}`} />,
+        },
+        {
+            path: "*",
+            element: <Navigate replace to={`/${SECURE_GUARD}/${CONTROL}`} />,
+        },
+    ],
+    { basename: base_url },
 );
 
 export default router;
--- src/router/RouterMenu.tsx
+++ src/router/RouterMenu.tsx
@@ -1,32 +1,32 @@
-import { CONTROL, MONITOR, SAFE_TREE, SECURITY_GUARD } from "../utils/constant";
+import { CONTROL, MONITOR, SAFE_TREE, SECURE_GUARD } from "../utils/constant";
 
 export type RouteType = {
-  id: string;
-  url: string;
-  label: string;
+    id: string;
+    url: string;
+    label: string;
 };
 
 export const constMenus: RouteType[] = [
-  {
-    id: "security-guard-monitor",
-    url: `/${SECURITY_GUARD}/${MONITOR}`,
-    label: "Security Guard Monitor",
-  },
-  {
-    id: "security-guard-control",
-    url: `/${SECURITY_GUARD}/${CONTROL}`,
-    label: "Security Guard Control",
-  },
-  {
-    id: "safe-tree-monitor",
-    url: `/${SAFE_TREE}/${MONITOR}`,
-    label: "SafeTree Monitor",
-  },
-  {
-    id: "safe-tree-control",
-    url: `/${SAFE_TREE}/${CONTROL}`,
-    label: "SafeTree Control",
-  },
+    {
+        id: "secure-guard-monitor",
+        url: `/${SECURE_GUARD}/${MONITOR}`,
+        label: "Secure Guard Monitor",
+    },
+    {
+        id: "secure-guard-control",
+        url: `/${SECURE_GUARD}/${CONTROL}`,
+        label: "Secure Guard Control",
+    },
+    {
+        id: "safe-tree-monitor",
+        url: `/${SAFE_TREE}/${MONITOR}`,
+        label: "SafeTree Monitor",
+    },
+    {
+        id: "safe-tree-control",
+        url: `/${SAFE_TREE}/${CONTROL}`,
+        label: "SafeTree Control",
+    },
 ];
 
 //export const RouteMenu = ()
