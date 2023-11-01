a0c496c345f71e1e528f336580eb533b7367106a
arin00github <teru325@naver.com>
2023-11-01 15:48:00 +0900

    ✨ Feat: [Map] 기능 컴포넌트 생성

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Map = () => {
+    return (
+        <Box w="100%" h="calc(100vh - 50px)" bg="blue.700">
+            <Box>map</Box>
+        </Box>
+    );
+};
+
+export default Map;
